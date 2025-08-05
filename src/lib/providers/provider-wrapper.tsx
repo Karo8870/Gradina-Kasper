4cae64260519008b7b582eef93fc4ef3a4e11527
karo <cazacuchristian@gmail.com>
2025-08-05 14:39:51 +0300

    update

--- src/lib/providers/provider-wrapper.tsx
+++ src/lib/providers/provider-wrapper.tsx
@@ -6,7 +6,7 @@ import { HeroUIProvider } from '@heroui/react';
 
 export default function ProviderWrapper({ children }: { children: ReactNode }) {
   return (
-    <HeroUIProvider className='flex h-screen w-screen flex-col items-center'>
+    <HeroUIProvider className='flex h-full w-screen flex-col items-center'>
       <BasketContextProvider>{children}</BasketContextProvider>
     </HeroUIProvider>
   );
