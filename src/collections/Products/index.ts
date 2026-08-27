a206a0da0984a7483e0b1bb22390a8f337e7e1dc
Karo8870 <77077774+Karo8870@users.noreply.github.com>
2026-08-27 15:57:57 +0300

    Update index.ts

--- src/collections/Products/index.ts
+++ src/collections/Products/index.ts
@@ -15,7 +15,7 @@ function validatePrice(value: string | null | undefined | string[]) {
     return "Value isn't a valid number with 2 decimal places";
   }
 
-  if (value.match(/^([^0]\d+|0)\.\d{2}$/g)) {
+  if (value.match(/^([^0]\d*|0)\.\d{2}$/g)) {
     return true;
   }
 
